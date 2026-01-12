c1959641a62aa036623956061325b27999741aec
Humk <bitrusseth06@gmail.com>
2026-01-12 12:21:05 +0100

    feat: add tags support and connect list command
    - Added --tags argument to add command
    - Connected list command to database
    - Formatted task output for readability
    - Improved user feedback messages

--- taskmaster/cli.py
+++ taskmaster/cli.py
@@ -1,6 +1,7 @@
 import argparse
-from taskmaster.database import add_task
+from taskmaster.database import add_task, all_tasks
 from taskmaster.database import db
+import json
 
 def main():
     parser = argparse.ArgumentParser()
@@ -16,19 +17,35 @@ def main():
     add.add_argument("--status", help='add a status to the project', default='todo')
     add.add_argument("--priority", help="add a prioriy", default="")
     add.add_argument("--due_date", help="add a due date")
+    add.add_argument("--tags", help="add a tag or tags")
+
+    
+
 
     args = parser.parse_args()
 
+    
+
+
     if args.command =="add":
+        tags_list = args.tags.split(',') if args.tags else []
+        tags_json = json.dumps(tags_list)
         task=add_task(db,
                       title=args.title,
                  description=args.description,
                  status=args.status,
                  priority=args.priority,
-                 due_date=args.due_date)
-        print(task)
+                 due_date=args.due_date,
+                 tags=tags_json)
+        print("task added successfully")
+    
     elif args.command == "list":
-        print("called list")
+        tasks = all_tasks(db)
+        print("All the items in the database")
+        print("="*100)
+        for task in tasks:
+            print(task)
+        
 
     def add_command():
         pass
